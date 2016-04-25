aaf18ee293fd5a6a8ab9793c217c1284f9633c05
Erik Brakke <eriksbrakke@gmail.com>
2016-04-24 21:59:11 -0400

    api scaffold

---
+++ api/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+});
+
+module.exports = app;
